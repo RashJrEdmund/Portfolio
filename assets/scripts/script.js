5a5b5a727f25f93f31424867ddaf783bdaa16984
rash <orashusedmund@gmail.com>
2022-12-15 03:59:20 +0100

    corrected linter errors

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,31 +1,31 @@
-const showButton = document.querySelector('.show-all-btn');
-const hiddenContainer = document.querySelectorAll('.varying');
+const showButton = document.querySelector('.show-all-btn')
+const hiddenContainer = document.querySelectorAll('.varying')
 
-let clicks = 0;
-console.log(showButton);
-console.log(hiddenContainer);
+let clicks = 0
+console.log(showButton)
+console.log(hiddenContainer)
 
 showButton.addEventListener('click', () => {
-    hiddenContainer.forEach(reappearing => {
-        console.log(hiddenContainer);
+  hiddenContainer.forEach(reappearing => {
+    console.log(hiddenContainer)
 
-        reappearing.classList.remove('hidden');
+    reappearing.classList.remove('hidden')
 
-        showButton.innerHTML = "<span></span>&#60; Show Less";
+    showButton.innerHTML = '<span></span>&#60; Show Less'
 
-        clicks += 1;
+    clicks += 1
 
-        console.log('current clicks =', clicks);
-        if (clicks > 1) {
-            addHidden ();
-            clicks = 0;
-        }
-    })
+    console.log('current clicks =', clicks)
+    if (clicks > 1) {
+      addHidden()
+      clicks = 0
+    }
+  })
 })
 
-function addHidden () {
-    hiddenContainer.forEach(reappearing => {
-        reappearing.classList.add('hidden');
-        showButton.innerHTML = "<span></span> Show More &#62";
-    })
+function addHidden() {
+  hiddenContainer.forEach(reappearing => {
+    reappearing.classList.add('hidden')
+    showButton.innerHTML = '<span></span> Show More &#62'
+  })
 }
